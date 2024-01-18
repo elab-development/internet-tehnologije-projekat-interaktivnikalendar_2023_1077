b02b3005ee5ac358cdacc7322b55e98a966850d1
Marija Mitrovic <mm20231077@student.fon.bg.ac.rs>
2024-01-18 18:09:16 +0100

    Initial code structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  const prom= <h1>Cao svima</h1>;
+  return (
+    
+    <div className="App">
+     <NavBar></NavBar>
+     {prom}
+     
+    </div>
+  );
+}
+
+export default App;
